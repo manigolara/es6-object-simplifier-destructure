11bae2c8a9503b283e5837dd4630366b3f8e20db
manigo <manigo>
2024-05-30 16:42:45 +0200

    v2

--- test.js
+++ test.js
@@ -1,30 +1,45 @@
-// OBJET SIMPLIFIE
-let nom = "franck";
-const objet = {nom};
-console.log(objet);
+/**
+ * OBJET SIMPLIFIE
+ * -------------------------------------------------------
+ */
 
+// exemple avec 1 params
+let nom = "franck";
+const objet = {nom}; // cle + propriete ex: {nom: nom}
+console.log(objet);
+console.log(objet.nom);
+
+// exemple avec 2 params
+let prenom = "Frank";
+let nom2 = "Herbert";
+
+const Personnage = {prenom, nom2};
+console.log(Personnage);
+
+// example avec les params d'une fonction
 function afficherNom({nom2, prenom}) {
   console.log(prenom + " " + nom2);
 }
 
-let prenom = "Frank";
-let nom2 = "Herbert";
-const Personnage = {prenom, nom2};
-console.log(Personnage);
-
 afficherNom({nom2, prenom});
 
+/**
+ * AFFECTATION DESTRUCTUREE
+ * -------------------------------------------------------
+ */
+
+// Version JSON Object
 const httpOptions = {timeout: 2000, key1: "maclé", isCache: true};
 
-const {timeout: httpTimeout, isCache: httpCache} = httpOptions;
-console.log(httpTimeout, httpCache);
+const {timeout: myTimeout, isCache: myIsCache} = httpOptions;
+console.log(myTimeout, myIsCache);
 
+// Version Tableau
 const httpOptions2 = [2000, true];
-const [httpTimeout2, httpCache2] = httpOptions2;
-console.log(httpTimeout2, httpCache2);
+const [myTimeout2, myIsCache2] = httpOptions2;
+console.log(myTimeout2, myIsCache2);
 
 // Example Meteo
-
 const {
   city_info: {name: maVille},
   current_condition: {date: maDate},
